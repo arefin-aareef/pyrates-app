4d56a79df2194742954579450a05b796b7f1dfbe
arefin_aareef <arefin.aareef@gmail.com>
2022-12-28 00:21:17 +0600

    putting components inside JSX

--- src/App.js
+++ src/App.js
@@ -4,13 +4,23 @@ import Footer from './Footer';
 import Home from './Home';
 import NewPost from './NewPost';
 import Missing from './Missing';
+import PostPage from './PostPage';
+import About from './About';
 import { Route, Switch, useHistory } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 function App() {
   return (
     <div className="App">
-      
+       <Header />
+       <Nav />
+       <Home />
+       <NewPost />
+       <PostPage />
+       <About />
+       <Missing />
+       <Footer />
+
     </div>
   );
 }
