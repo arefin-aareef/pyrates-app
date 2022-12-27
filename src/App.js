e01e036c5032cfbeab5a0cc15c35ac21e4017fe3
arefin_aareef <arefin.aareef@gmail.com>
2022-12-28 00:06:48 +0600

    imported necessary components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-
+import Header from './Header';
+import Nav from './Nav';
+import Footer from './Footer';
+import Home from './Home';
+import NewPost from './NewPost';
+import Missing from './Missing';
 
 function App() {
   return (
