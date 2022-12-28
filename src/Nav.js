ab880aed4d8198c8b4f1d519578a5488f755cb97
arefin_aareef <arefin.aareef@gmail.com>
2022-12-28 17:17:43 +0600

    search form added in nav

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
-const Nav = () => {
+const Nav = ({ search, setSearch }) => {
   return (
-    <nav>
-        <h1>Nav</h1>
+    <nav className='Nav'>
+        <form className='searchForm' onSubmit={(e) => e.preventDefault()}>
+          <label htmlFor='search'>Search Posts</label>
+          <input
+            id='search'
+            type='text'
+            placeholder='Search Posts'
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </form>
     </nav>
   )}
 
