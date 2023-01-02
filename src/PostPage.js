5dc762cb66189f6258e1a1c753c8a521c66e62dc
arefin_aareef <arefin.aareef@gmail.com>
2023-01-02 18:36:42 +0600

    Post exists or not

--- src/PostPage.js
+++ src/PostPage.js
@@ -3,11 +3,11 @@ import { useParams, Link } from "react-router-dom";
 
 const PostPage = ({ posts, handleDelete }) => {
   const { id } = useParams();
-  const post = posts.find(post => (post.id).toSring() === id);
+  const post = posts.find(post => (post.id).toString() === id);
   return (
     <main className='PostPage'>
         <article className='post'>
-          {post && 
+          {post && // if post page exists
             <>
               <h2>{post.title}</h2>
               <p className='postDate'>{post.datetime}</p>
@@ -17,6 +17,16 @@ const PostPage = ({ posts, handleDelete }) => {
               </button>
             </>
           }
+          {!post && // if post page not exists
+            <>
+              <h2>Post Not Found</h2>
+              <p>Well, that's disappointing</p>
+              <p>
+                <Link to='/'>Visit Our Houmepage</Link>
+              </p>
+            </>
+          }
+
         </article>
     </main>
   )
