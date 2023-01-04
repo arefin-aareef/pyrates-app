1152be3c451cdc2aef0ec5c623de9abe07560e57
arefin_aareef <arefin.aareef@gmail.com>
2023-01-05 01:31:37 +0600

    edit post imported

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Layout from './Layout';
 import Home from './Home';
 import NewPost from './NewPost';
 import PostPage from './PostPage';
+import EditPost from './EditPost';
 import About from './About';
 import Missing from './Missing';
 import { Route, Routes, useNavigate } from 'react-router-dom';
@@ -107,6 +108,16 @@ function App() {
           <Route path="/post/:id" element={<PostPage 
             posts={posts} 
             handleDelete={handleDelete} 
+          />} />
+        </Route>
+        <Route path='/edit/:id'>
+          <Route index element={<EditPost
+            posts={posts}
+            handleEdit={handleEdit}
+            editTitle={editTitle}
+            setEditTitle={setEditTitle}
+            editBody={editBody}
+            setEditBody={setEditBody}
           />} />
         </Route>
         <Route path="about" element={<About />} />
